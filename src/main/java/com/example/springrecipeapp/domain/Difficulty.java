b402fb149d3d6b86c3ea3f5f2de11faae4eb2fe2
Your Name <you@example.com>
2021-03-17 20:44:12 -0300

    Git Update to Mysql

--- src/main/java/com/example/springrecipeapp/domain/Difficulty.java
+++
@@ -1,5 +0,0 @@
-package com.example.springrecipeapp.domain;
-
-public enum  Difficulty {
-    EASY, MODERATE, HARD
-}
--- src/main/java/com/example/springrecipeapp/repositories/RecipeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springrecipeapp.repositories;
-
-import com.example.springrecipeapp.domain.Recipe;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
-}
